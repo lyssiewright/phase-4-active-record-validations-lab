7c68ae6f522156f01c1e5f413cefb1e4e96f6879
Lyssie <lyssiewright.15@gmail.com>
2022-04-05 08:55:18 -0700

    commit tree 8b1f2d2bc2e515e50e357fad2d957917dbb02f5e

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+
+
+
+    def create
+        author = Author.create(author_params)
+        render json: author, status: :created
+      end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250} 
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_cannot_be_true_facts
+
+  def title_cannot_be_true_facts
+    if :title === "True Facts"
+      errors.add(:title, "Can't be that title")
+    end
+  end
+
+
+    def create
+        post = Post.create(post_params)
+        render json: post, status: :created
+      end
 end
